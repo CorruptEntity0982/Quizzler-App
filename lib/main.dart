a4af4ea23777e78570b4ca9ebf18914de9bf6d57
Shashank Dubey <shashank02.dubey@gmail.com>
2023-03-10 18:30:42 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(app);
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+class  extends StatefulWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
