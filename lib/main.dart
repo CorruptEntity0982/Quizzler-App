1dcd7fd52b4cf48fbc84239616c4cfa302743e19
Shashank Dubey <shashank02.dubey@gmail.com>
2023-03-10 19:10:29 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,98 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(app);
+void main() => runApp(
+      const QuizApp(),
+    );
 
-class  extends StatelessWidget {
-  const ({Key? key}) : super(key: key);
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black54,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: Quizpage(),
+        ),
+      ),
+    );
   }
 }
 
-
-class  extends StatefulWidget {
-  const ({Key? key}) : super(key: key);
+class Quizpage extends StatefulWidget {
+  const Quizpage({Key? key}) : super(key: key);
 
   @override
-  State<> createState() => _State();
+  State<Quizpage> createState() => _QuizpageState();
 }
 
-class _State extends State<> {
+class _QuizpageState extends State<Quizpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Expanded(
+              flex: 5,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    'This is where the questions will go',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.green),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'True',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.red),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'False',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
-
